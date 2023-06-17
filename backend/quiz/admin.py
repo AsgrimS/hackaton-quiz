b46433d5d25a636f96b10a7e2097c137afa3d761
Łukasz Wolak <lukasz.wolak@theluke.dev>
2023-06-17 10:47:43 +0200

    generation of questions WIP

--- backend/quiz/admin.py
+++ backend/quiz/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+from quiz.models import OpenQuestion, ClosedQuestion, Quiz
+
 # Register your models here.
+admin.site.register([Quiz, OpenQuestion, ClosedQuestion])
--- backend/quiz/models.py
+++ backend/quiz/models.py
@@ -9,6 +9,40 @@ class Quiz(models.Model):
     def __str__(self):
         return self.title
 
+    @classmethod
+    def create_new(cls, title, description, number_of_closed_questions, number_of_open_questions):
+        quiz = cls(title=title, description=description)
+        quiz._generate_open_questions(number_of_open_questions)
+        quiz._generate_closed_questions(number_of_closed_questions)
+        quiz.save()
+        return quiz
+
+    def _generate_closed_questions(self, number_of_closed_questions, ):
+        for _ in range(number_of_closed_questions):
+            ClosedQuestion.objects.create(
+                quiz=self,
+                question_text=...,
+                question_number=...,
+                points=...,
+                answer_1=...,
+                answer_2=...,
+                answer_3=...,
+                answer_4=...,
+                correct_answer_1=...,
+                correct_answer_2=...,
+                correct_answer_3=...,
+                correct_answer_4=...,
+            )
+
+    def _generate_open_questions(self, number_of_open_questions):
+        for _ in range(number_of_open_questions):
+            OpenQuestion.objects.create(
+                quiz=self,
+                question_text=...,
+                question_number=...,
+                valid_response=...,
+            )
+
 
 class ClosedQuestion(models.Model):
     question_text = models.TextField()
@@ -32,7 +66,6 @@ class OpenQuestion(models.Model):
     question_text = models.TextField()
     quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
     question_number = models.IntegerField(default=1)
-
     valid_response = models.TextField()
 
     def __str__(self):
