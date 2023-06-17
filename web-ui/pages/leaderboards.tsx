4b7eaf953cf9197ca64ab5ebd1237c1bd5996269
MarcinParda <marcin98parda@gmail.com>
2023-06-17 11:47:18 +0200

    feat: added search to leaderboards

--- web-ui/pages/leaderboards.tsx
+++ web-ui/pages/leaderboards.tsx
@@ -1,5 +1,28 @@
 import { Leaderboard } from "@/components/Leaderboard";
+import { useState } from "react";
+import Select from "react-select";
+
+interface Option {
+  value: string;
+  label: string;
+}
+
+const options: Option[] = [
+  { value: "chocolate", label: "Chocolate" },
+  { value: "strawberry", label: "Strawberry" },
+  { value: "vanilla", label: "Vanilla" },
+];
 
 export default function Leaderboards() {
-  return <Leaderboard />;
+  const [selectedOption, setSelectedOption] = useState<Option | null>(null);
+  return (
+    <div>
+      <Select
+        options={options}
+        value={selectedOption}
+        onChange={setSelectedOption}
+      />
+      <Leaderboard />
+    </div>
+  );
 }
