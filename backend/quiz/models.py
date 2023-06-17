f210c52b76895da56910f7259c738ae0f010d771
Łukasz Wolak <lukasz.wolak@theluke.dev>
2023-06-17 10:30:10 +0200

    quiz models

--- backend/quiz/models.py
+++ backend/quiz/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
-# Create your models here.
+
+class Quiz(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    is_published = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+
+class ClosedQuestion(models.Model):
+    question_text = models.TextField()
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    question_number = models.IntegerField(default=1)
+    points = models.IntegerField(default=0)
+    answer_1 = models.TextField()
+    answer_2 = models.TextField()
+    answer_3 = models.TextField()
+    answer_4 = models.TextField()
+    correct_answer_1 = models.BooleanField(default=False)
+    correct_answer_2 = models.BooleanField(default=False)
+    correct_answer_3 = models.BooleanField(default=False)
+    correct_answer_4 = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.question_text
+
+
+class OpenQuestion(models.Model):
+    question_text = models.TextField()
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    question_number = models.IntegerField(default=1)
+
+    valid_response = models.TextField()
+
+    def __str__(self):
+        return self.question_text
